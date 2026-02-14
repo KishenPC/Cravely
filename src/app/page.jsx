b395307051388eb75673fd7ef48948f0639c847f
MKDev121 <meatk121@gmail.com>
2026-02-14 22:45:40 +0530

    Added Plate Button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
+import FoodListItem from "./Components/FoodListItem";
 import MenuPage from "./Components/MenuPage";
+import PlateButton from "./Components/PlateButton";
 
 export default function Home() {
   return (
     <main className="flex-1 flex-col content-around justify-items-center border-2 h-screen bg-[url(Assets/WebpageBackground.png)] bg-size-[100vw_100vh]">
-      <MenuPage />
+      <MenuPage >
+        <FoodListItem />
+        <PlateButton />
+      </MenuPage>
     </main>
-  )
+  );
 }
